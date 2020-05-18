34cbc7be1166a07e9a6c5498d1df9065606c2d07
qiangayi <30819256+qiangayi@users.noreply.github.com>
2020-05-18 17:09:51 +0800

    1

--- api/teach.js
+++ api/teach.js
@@ -2,7 +2,7 @@ import request from "@/common/request.js"
 
 //获取视频提问列表
 export function range(data){
-	return request("/teach/AskRange", "GET", data)
+	return request("/teach/wxAskRange", "GET", data)
 }
 //新增提问
 export function addAsk(data){
