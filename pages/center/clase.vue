79326f62f5cf8978c726d4c91cca900bacf9b9b8
qiangayi <30819256+qiangayi@users.noreply.github.com>
2020-06-03 22:56:11 +0800

    1

--- pages/center/clase.vue
+++ pages/center/clase.vue
@@ -1,7 +1,40 @@
 <template>
 	<view>
-		<view class="cu-list menu-avatar ">
-			<block v-for="clase in data" :key="clase.claseName">
+		<uni-collapse accordion="true">
+			<uni-collapse-item :title="clase.claseName" v-for="clase in data" :key="clase.claseName">
+				<uni-collapse accordion="true">
+					<uni-collapse-item :title="type == 'seller' ? video.videoName : video.videoName  + ' ' + video.complete" v-for="video in clase.videoData" :key="clase.claseName">
+						<view class="cu-list menu-avatar ">
+							<view class="cu-bar solid-bottom" v-if="type == 'seller'">
+								<view class="action">
+									<text class="cuIcon-titles text-green"></text>
+									<text class="text-lg text-bold">待学习学员</text>
+								</view>
+							</view>
+							<block v-for="stu in video.stuData" :key="stu.Id">
+								<view class="cu-item" v-if="type == 'seller' && stu.StartTime == null">
+									<view class="content">
+										<text class="text-grey text-xl">{{stu.Name}}</text>
+									</view>
+									<view class="action flex justify-end padding-right-xs">
+										<text class="text-grey text-xl">{{stu.Mobile}}</text>
+									</view>
+								</view>
+								<view class="cu-item" v-if="type == 'client'">
+									<view class="content">
+										<text class="text-grey text-xl">{{stu.Name + "    " + stu.Gender + "    " + stu.Age + "岁 "  }}</text>
+									</view>
+									<!-- <view class="action flex justify-end padding-right-xs">
+										<text class="text-grey text-xl">课程总完成度{{stu.DutyPercent}}</text>
+									</view> -->
+								</view>
+							</block>
+						</view>
+					</uni-collapse-item>
+				</uni-collapse>
+			</uni-collapse-item>
+		</uni-collapse>
+		<!-- <block v-for="clase in data" :key="clase.claseName">
 				<view class="cu-bar solid-bottom">
 					<view class="action">
 						<text class="cuIcon-titles text-green"></text>
@@ -36,12 +69,13 @@
 						</template>
 					</view>
 				</block>
-			</block>
-		</view>
+			</block> -->
 	</view>
 </template>
 
 <script>
+	import uniCollapse from '@/components/uni-collapse/uni-collapse.vue'
+	import uniCollapseItem from '@/components/uni-collapse-item/uni-collapse-item.vue'
 	import {
 		clasesInfo
 	} from "@/api/clase.js"
@@ -61,6 +95,10 @@
 				clases: '',
 				data: []
 			}
+		},
+		components: {
+			uniCollapse,
+			uniCollapseItem
 		},
 		computed: {
 			...mapGetters(['sellerClase', 'clientClase']),
